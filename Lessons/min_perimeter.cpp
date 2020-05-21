1f3cdfa8a1c190884248a414bd6fec37fb86ac79
ShabanovR <55501178+ShabanovR@users.noreply.github.com>
2020-05-21 23:38:28 +0800

    Update min_perimeter.cpp

--- Lessons/min_perimeter.cpp
+++ Lessons/min_perimeter.cpp
@@ -1,31 +1,33 @@
 #include <iostream>
-#include <algorithm>
-#include <climits>
 #include <fstream>
+#include <algorithm>
 #include <ctime>
 using namespace std;
 
+struct point {
+	int x, y, id;
+};
 
-struct point { int x, y, id; };
+inline bool cmp_x(const point &a1, const point &a2) {
+	return a1.x < a2.x || a1.x == a2.x && a1.y < a2.y;
+}
 
-inline bool cmp_x(const point& a1, const point& a2) { return a1.x < a2.x || a1.x == a2.x && a1.y < a2.y; }
-
-inline bool cmp_y(const point& a1, const point& a2) { return a1.y < a2.y; }
+inline bool cmp_y(const point &a1, const point &a2) {
+	return a1.y < a2.y;
+}
 
 int N;
 point* a;
 point* temp;
-double perimeter = DBL_MAX;
 point triangle[3];
+double minper = DBL_MAX;
 
-inline double distance(const point& a1, const point& a2) {
-	return sqrt((a1.x - a2.x)*(a1.x - a2.x) + (a1.y - a2.y)*(a1.y - a2.y) + 0.0);
-}
-
-inline void applicant(const point& a1, const point& a2, const point& a3) {
-	double per = distance(a1, a2) + distance(a1, a3) + distance(a2, a3);
-	if (per < perimeter) {
-		perimeter = per;
+inline void upd_id(const point &a1, const point &a2, const point &a3) {
+	double per = sqrt((a1.x - a2.x)*(a1.x - a2.x) + (a1.y - a2.y)*(a1.y - a2.y))
+		+ sqrt((a1.x - a3.x)*(a1.x - a3.x) + (a1.y - a3.y)*(a1.y - a3.y))
+		+ sqrt((a3.x - a2.x)*(a3.x - a2.x) + (a3.y - a2.y)*(a3.y - a2.y));
+	if (per < minper) {
+		minper = per;
 		triangle[0] = a1;
 		triangle[1] = a2;
 		triangle[2] = a3;
@@ -34,21 +36,25 @@ inline void applicant(const point& a1, const point& a2, const point& a3) {
 
 void rec(int l, int r) {
 	if (r - l <= 3) {
-		applicant(a[l], a[l + 1], a[r]);
+		if (r - l == 3)
+			upd_id(a[l], a[l + 1], a[r]);
+		sort(a + l, a + r + 1, &cmp_y);
 		return;
 	}
 
 	int m = (l + r) >> 1;
-	point mid = a[m];
+	int midx = a[m].x;
 	rec(l, m), rec(m + 1, r);
+
+	merge(a + l, a + m + 1, a + m + 1, a + r + 1, temp, &cmp_y);
 
 	copy(temp, temp + r - l + 1, a + l);
 
 	int tsz = 0;
-	for (int i = l; i <= r; i++)
-		if (distance(mid, a[i]) <= (perimeter / 2)) {
-			for (int j = tsz - 1; j >= 0 && distance(temp[j], a[i]) < (perimeter / 2); j--)
-				applicant(a[i], temp[j], mid);
+	for (int i=l;i<=r;i++)
+		if (abs(a[i].x - midx) <= (minper / 2) ) {
+			for (int j = tsz - 2; j >= 0 && (a[i].y - temp[j].y) < (minper / 2) && (a[i].y - temp[j + 1].y) < (minper / 2); j -= 2)
+				upd_id(a[i], temp[j], temp[j+1]);
 			temp[tsz++] = a[i];
 		}
 }
@@ -82,7 +88,7 @@ time_t example1(int N, bool flag) {
 			in << a[i].id << '\t' << a[i].x << '\t' << a[i].y << '\n';
 		in.close();
 
-		cout << "Min_perimeter: " << perimeter << endl;
+		cout << "Min_perimeter: " << minper << endl;
 		cout << "ID\tX\tY\n";
 		for (point t : triangle) {
 			cout << t.id << '\t' << t.x << '\t' << t.y << endl;
@@ -96,14 +102,15 @@ time_t example1(int N, bool flag) {
 	return end - start;
 }
 
+
 int main() {
-	srand(time_t(NULL));
+	srand(time(NULL));
 
 	ofstream rez_time("timer_algo.dat");
 
 	int pow10 = 10;
 	for (int raz = 2; raz <= 6; raz++) {
-		cout << "raz = " << raz << '\t';
+		cout << "raz: " << raz << '\t';
 		pow10 *= 10;
 		for (int i = 1; i < 10; i++) {
 			cout << i << ' ';
@@ -111,9 +118,9 @@ int main() {
 			time_t t = example1(N, false);
 			rez_time << N << '\t' << t << '\n';
 		}
-		cout << endl;
 	}
 	rez_time.close();
-	cin.get();
+
+	system("pause");
 	return 0;
 }
